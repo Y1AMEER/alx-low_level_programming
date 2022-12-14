8a53f80c0d7f32713f87f030af9db437cf0790b8
Y1AMEER <adeyemiyusuf41@gmail.com>
2022-12-14 10:07:46 -0800

    1-main.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all possible different combination of three digits.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ones = '0';
+	jnt tens = '0';
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundred <= '9'; hundred++)
+	{
+		for (tens = '0'; tens <= '9' tens++)
+		{
+			for (ones  = '0'; ones <= '9'; ones++
+			
