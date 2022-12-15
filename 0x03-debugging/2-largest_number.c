f6ef662b2d78db4a406159e89e88ef0a98211f06
Y1AMEER <adeyemiyusuf41@gmail.com>
2022-12-15 09:17:29 -0800

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,21 +14,20 @@ int largest;
 
 if (a > b && a > c)
 {
-	lagest = a;
+largest = a;
 }
-else if (b > c && b > a)
+else if (a > b && c > a)
 {
-	largest = b;
+largest = c;
 }
-else if (c > b)
+else if (b > c)
 {
-	largest = c;
+largest = b;
 }
 else
 {
-	largest = b;
+largest = c;
 }
 
 return (largest);
 }
-
