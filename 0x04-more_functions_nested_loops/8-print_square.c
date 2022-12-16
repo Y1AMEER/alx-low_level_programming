d01b6bd7eaa0d638f92ee507162f3ba952cb63c4
Y1AMEER <adeyemiyusuf41@gmail.com>
2022-12-16 08:53:52 -0800

    08

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,7 +4,6 @@
  * print_square - prints a square, followed by a new line;
  * @size: size of the square
  */
-
 void print_square(int size)
 {
 if (size <= 0)
@@ -19,7 +18,7 @@ for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
-_putchar('0');
+_putchar('#');
 }
 _putchar('\n');
 }
